9c90194d0f32287c33c91a20184e6d8e1f21fcd4
Kristina <111386621+kristina-bellrich@users.noreply.github.com>
2024-01-15 17:12:48 +0100

    Update TodoController.js

--- TodoController.js
+++ TodoController.js
@@ -1,29 +1,30 @@
-const AppModel = require('./AppModel')
+const TodoModel = require('./TodoModel');
 
-    module.exports.getTodo = async (req, res) =>{
-        const myTodo = await TodoModel.find();
-        res.send(myTodo)
-    }
 
-    module.exports.saveTodo = async (req, res) =>{
-        const {title} = req.body;
-        AppModel.create({title})
-        .then((data)=> {console.log("Todo added")
+module.exports.getTodo = async(req, res)=>{
+    const myApp= await TodoModel.find();
+    res.send(myApp)
+}
+
+module.exports.saveTodo = async(req, res)=>{
+    const {title}=req.body;
+    TodoModel.create({title})
+     .then((data)=>{console.log("added!")
     res.send(data)
     })
-    }
+}
 
+module.exports.deleteTodo = async(req, res)=>{
+    const {_id}=req.body;
+    TodoModel.findByIdAndDelete(_id)
+     .then(()=>{
+    res.send("deleted")
+    })
+}
 
-    module.exports.deleteTodo = async (req, res) =>{
-        const {_id} = req.body;
-        AppModel.findByIdAndDelete(_id)
-        .then(()=> res.send("Todo deleted"))
-    }
-
-
-
-    module.exports.editTodo = async (req, res) =>{
-        const {_id, title} = req.body;
-        AppModel.findByIdAndUpdate(_id, {title})
-        .then(()=> res.send("Edited a Todo"))
-    }
+module.exports.editTodo = async(req, res)=>{
+    const {_id, title}=req.body;
+    TodoModel.findByIdAndUpdate(_id,{title})
+     .then(()=>{
+    res.send("edited")
+    })}
