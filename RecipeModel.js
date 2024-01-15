68ad845896be45ebebac80de8b3994c98973f9aa
Kristina <111386621+kristina-bellrich@users.noreply.github.com>
2024-01-15 13:41:45 +0100

    Update RecipeModel.js

--- RecipeModel.js
+++ RecipeModel.js
@@ -1,27 +1,8 @@
 const mongoose = require('mongoose')
-const appShema = new mongoose.Schema({
-    title: {
-    type: String,
-    required: true
-    },
-    description: {
-        type: String
-        },
-    image: {
-        type: String
-        }
-})
-
-module.exports= mongoose.model("FamilyApp", appShema)
-
-
 
 
 const TodoShema = new mongoose.Schema({
-    title: String,
-    ingredients: String,
-    dayOfWeek : String,
-    typeOfMeal : String
+    title: String
 })
 
 module.exports= mongoose.model("TodoList", TodoShema)
