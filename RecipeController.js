7bab1759dd9613c7fbfff4d707e67bbc481c1e98
Kristina <111386621+kristina-bellrich@users.noreply.github.com>
2024-01-15 13:40:06 +0100

    Update RecipeController.js

--- RecipeController.js
+++ RecipeController.js
@@ -1,60 +1,32 @@
 const AppModel = require('./AppModel')
 
-module.exports.getRecipe = async(req, res)=>{
-    const myApp= await AppModel.find();
-    res.send(myApp)
-}
-
-module.exports.saveRecipe = async(req, res)=>{
-    const {title, description, image}=req.body;
-     AppModel.create({title, description, image})
-     .then((data)=>{console.log("added!")
-    res.send(data)
-    })
-}
-
-module.exports.deleteRecipe = async(req, res)=>{
-    const {_id}=req.body;
-     AppModel.findByIdAndDelete(_id)
-     .then(()=>{
-    res.send("deleted")
-    })
-}
-
-module.exports.editRecipe = async(req, res)=>{
-    const {_id, title, description,image}=req.body;
-     AppModel.findByIdAndUpdate(_id,{title, description, image})
-     .then(()=>{
-    res.send("edited")
-    })}
 
 
+    module.exports.getTodo = async (req, res) =>{
+        const myMeal = await TodoModel.find();
 
-    module.exports.getMeal = async (req, res) =>{
-        const myMeal = await MealModel.find();
-
-        res.send(myMeal)
+        res.send(myTodo)
     }
 
-    module.exports.saveMeals = async (req, res) =>{
-        const {title, ingredients,dayOfWeek, typeOfMeal} = req.body;
-        MealModel.create({title , ingredients,dayOfWeek,typeOfMeal})
-        .then((data)=> {console.log("Meal added")
+    module.exports.saveTodo = async (req, res) =>{
+        const {title} = req.body;
+        MealModel.create({title})
+        .then((data)=> {console.log("Todo added")
     res.send(data)
     })
     }
 
 
-    module.exports.deleteMeal = async (req, res) =>{
+    module.exports.deleteTodo = async (req, res) =>{
         const {_id} = req.body;
-        MealModel.findByIdAndDelete(_id)
-        .then(()=> res.send("Meal deleted"))
+        TodoModel.findByIdAndDelete(_id)
+        .then(()=> res.send("Todo deleted"))
     }
 
 
 
-    module.exports.editMeal = async (req, res) =>{
-        const {_id, title, ingredients,dayOfWeek, typeOfMeal} = req.body;
-        MealModel.findByIdAndUpdate(_id, {title, ingredients,dayOfWeek,typeOfMeal})
-        .then(()=> res.send("Edited a meal"))
+    module.exports.editTodo = async (req, res) =>{
+        const {_id, title} = req.body;
+        MealModel.findByIdAndUpdate(_id, {title})
+        .then(()=> res.send("Edited a Todo"))
     }
