5c7f2ca01ec7e528b51cb48ef18eec50110503a3
Kristina <111386621+kristina-bellrich@users.noreply.github.com>
2024-01-16 14:17:56 +0100

    Update TodoModel.js

--- TodoModel.js
+++ TodoModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const appShema = new mongoose.Schema({
-    title: {
-    type: String,
-    required: true
-    },
-
+    title: String,
 })
 
 module.exports= mongoose.model("TodoList", appShema)
